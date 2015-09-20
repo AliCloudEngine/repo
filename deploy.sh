64728edcf59161600e8c14a2ce3200c9321fb7c5
硕根 <shuhong.dongsh@alibaba-inc.com>
2015-09-20 17:14:35 +0800

    doc: task api dependencies

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ find com -name .DS_Store -delete
 
 {
 	cat head.txt
-	find com -type f -not -wholename 'com/aliyun/openservices/*' | sed 's#.*#<a href="./\0">./\0</a><br/>#'
+	find com -type f -not -wholename 'com/aliyun/openservices/*' | awk '{print "<a href=\"./"$1"\">./"$1"</a><br/>"}'
 	cat tail.txt
 } > index.html
